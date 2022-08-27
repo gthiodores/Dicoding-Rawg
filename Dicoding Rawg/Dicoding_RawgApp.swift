0adca609e892e5cb7c706954c5ae78ae1d42e707
Gilbert Thiodores <gthiodores71@gmail.com>
2022-08-27 16:49:26 +0800

    Initial Commit

---
+++ Dicoding Rawg/Dicoding_RawgApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicoding_RawgApp.swift
+//  Dicoding Rawg
+//
+//  Created by Lucy on 27/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicoding_RawgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
